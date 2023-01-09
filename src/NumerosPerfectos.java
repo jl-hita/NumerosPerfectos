87aa2d7c959a73ef52f569f4919e68a599b151a9
Jose Luis Hita <jl.hitap@gmail.com>
2023-01-09 17:54:27 +0100

    Primer Commit

---
+++ src/NumerosPerfectos.java
@@ -0,0 +1,20 @@
+public class NumerosPerfectos {
+	public static void main(String[] args) {
+		System.out.println("Números perfectos entre 1 y 10000000: ");
+		for(int i=1;i <= 10000000;i++){
+        	if(esPerfecto(i))
+        		System.out.println(i);
+        }
+	}
+	static boolean esPerfecto(int n) {
+		int suma=0;
+		boolean perfecto=false;
+		for(int i = 1; i < n; i++) {                            
+			if (n % i == 0)
+				suma = suma + i;
+		}
+		if (suma == n)                
+			perfecto=true;
+		return perfecto;
+	}
+}
